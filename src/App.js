62a6845096f8f905d5fc7003bb0f9d9a3a2f8950
Hassan Habib Tahir <hassanhabibtahir@gmail.com>
2020-09-01 06:43:59 +0500

    animation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useRef,useEffect} from 'react';
+import useWebAnimations from "@wellyshen/use-web-animations";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  const {ref,playState,getAnimation} = useWebAnimations({
+    keyframes:[
+      {transform:"translateY(0px)" , backgroundColor:"blue"},
+      {transform:"translateY(500px)",backgroundColor:"green"}
+    ],
+    timing: {
+      delay: 500, // Start with a 500ms delay
+      duration: 500, // Run for 1000ms
+      iterations: Infinity, // Repeat once
+      direction: "alternate", // Run the animation forwards and then backwards
+      easing: "ease-in-out", // Use a fancy timing function
+    },
+  })
+
+
+
+
+  return ( 
+     <div><h1 ref={ref} style={{
+       backgroundColor:"red",width:"200px",height
+      :"200px"
+     }}>How ARE GOOD</h1>
+  <h1>Animatino play state{playState}</h1>
+  
+  <button onClick={()=>{
+    getAnimation().play()
+  }}>play</button>
+  <button onClick={()=>{
+    getAnimation().pause()
+  }}>pause</button>
+  
+     </div>
   );
 }
 
 export default App;
+
